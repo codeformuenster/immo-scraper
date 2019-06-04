479d5f9e5899aaf4ec33a088194f1622f1b24c04
Thorben Jensen <jensen.thorben@gmail.com>
2019-06-04 22:13:49 +0200

    adding logger to main script

--- scraper.py
+++ scraper.py
@@ -1,12 +1,22 @@
 """ Main script that executes all scrapers. """
 
+import logging
 import sys
 
 sys.path.append(".")
 from immo_scraper import nestoria  # noqa: E402
 
+# set up logging
+logging.basicConfig(stream=sys.stdout, level=logging.INFO)
+
+log = logging.getLogger("info_logger")
+log.setLevel(logging.INFO)
+log.info("Logger set up.")
+
 # scrape
+log.info("Scraping...")
 nestoria_result = nestoria.scrape()
+log.info("Scraping done.")
 
 # save to S3 bucket
 # TODO
