9d13b0c851b24aa3dd9a4abe72724baa8961a587
Thorben Jensen <jensen.thorben@gmail.com>
2019-06-18 22:48:13 +0200

    adding utils for writing data to s3 bucket

--- immo_scraper/bucket.py
+++ immo_scraper/bucket.py
@@ -1,2 +1,38 @@
+import boto3
+
+BUCKET_NAME = "codeformuenster"
 
 
+def read_credentials():
+    with open("secret/bucket.txt", "r") as f:
+        creds = f.read().split("\n")
+        BUCKET_URL = creds[0]
+        BUCKET_SECRET = creds[1]
+        BUCKET_ID = creds[2]
+    return BUCKET_URL, BUCKET_SECRET, BUCKET_ID
+
+
+def get_bucket():
+    BUCKET_URL, BUCKET_SECRET, BUCKET_ID = read_credentials()
+    session = boto3.Session(region_name="fr-par")
+    s3 = session.resource(
+        service_name="s3",
+        endpoint_url=BUCKET_URL,
+        aws_access_key_id=BUCKET_ID,
+        aws_secret_access_key=BUCKET_SECRET,
+    )
+    bucket = s3.Bucket(BUCKET_NAME)
+    return bucket
+
+
+def print_bucket_files():
+    bucket = get_bucket()
+    for obj in bucket.objects.all():
+        key = obj.key
+        print(f"key: {key}")
+
+
+def write_to_s3(filename, content):
+    bucket = get_bucket()
+    key = "immoscout/" + filename
+    bucket.put_object(Bucket=BUCKET_NAME, Body=content, Key=key)
