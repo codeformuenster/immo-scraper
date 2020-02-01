f7a6523536c4b4c284357c67b314d36a7290813e
Thorben Jensen <jensen.thorben@gmail.com>
2020-02-01 21:03:31 +0100

    refactor alert script

---
+++ immo_scraper/alert/email.py
@@ -0,0 +1,36 @@
+import logging
+import os
+from typing import Text
+
+import pandas as pd
+
+from sendgrid import SendGridAPIClient
+from sendgrid.helpers.mail import Mail
+
+
+def compose_html(df_alerts: pd.DataFrame) -> Text:
+    logging.info("Composing email text...")
+    text = f"Found {len(df_alerts)} new relevant alerts:</br></br>"
+    for id in df_alerts.id:
+        link = df_alerts[df_alerts.id == id].lister_url.values[0]
+        text += f"- <a href='{link}'>offer</a>.</br>"
+    logging.debug(f"Email html: {text}")
+    return text
+
+
+def send(text: Text):
+    logging.info("Sending email...")
+    message = Mail(
+        from_email=os.environ["FROM_EMAIL"],
+        to_emails=os.environ["TO_EMAIL"],
+        subject="Immo-scraper: new relevant offer(s).",
+        html_content=text,
+    )
+    try:
+        sg = SendGridAPIClient(os.environ["EMAIL_API_TOKEN"])
+        response = sg.send(message)
+        logging.debug(response.status_code)
+        logging.debug(response.body)
+        logging.debug(response.headers)
+    except Exception as e:
+        logging.error(e.message)
---
+++ immo_scraper/alert/io.py
@@ -0,0 +1,18 @@
+""" I/O utils related to alerts. """
+
+from typing import Text
+
+from immo_scraper.io import paths
+
+
+def get_ids_previous_alerts() -> Text:
+    alerts_log = []
+    if paths.ALERTS_IDS.exists():
+        with open(str(paths.ALERTS_IDS), "r") as f:
+            alerts_log = f.read().splitlines()
+    return alerts_log
+
+
+def log_alert(id):
+    with open(str(paths.ALERTS_IDS), "a") as f:
+        f.write(id + "\n")
