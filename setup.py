545f7aed60c32b68a6fc47be86977dc4aff32ba2
Thorben Jensen <jensen.thorben@gmail.com>
2020-02-01 21:38:58 +0100

    release 0.2.0

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="immo_scraper",
-    version="0.1.0",
+    version="0.2.0",
     # about
     license="MIT",
     url="https://github.com/codeformuenster/immo-scout",
